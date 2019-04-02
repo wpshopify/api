1934b840e34da3a6df6469af62fa6940ca3a04dc
Andrew Robbins <arobbins@simpleblend.net>
2019-04-02 17:29:31 -0500

    Updated pricing and buy_button components

--- src/internal/components/index.js
+++ src/internal/components/index.js
@@ -15,12 +15,18 @@ Returns: promise
 async function getComponentOptions(componentOptionsIds = {}) {
    const cachedResult = getCache('wps-component-options-' + componentOptionsIds.data.join('-'))
 
+   console.log('cachedResult', cachedResult)
+
    if (cachedResult) {
+      console.log('ss')
+
       return Promise.resolve(cachedResult)
    }
-
+   console.log('zz')
    return new Promise(async (resolve, reject) => {
       const result = await post(endpointComponentOptions(), componentOptionsIds)
+
+      console.log('result ........ 🔥', result)
 
       setCache('wps-component-options-' + componentOptionsIds.data.join('-'), result.data)
 
--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,4 @@
-import { buildClient } from '../client';
-
-
+import { buildClient } from '../client'
 
 /*
 
@@ -8,29 +6,28 @@ Direct API functions
 
 */
 function fetchProductByID(id, client) {
-   return client.product.fetch(id);
+   return client.product.fetch(id)
 }
 
 function fetchProductsByIDs(ids, client) {
-   return client.product.fetchMultiple(ids);
+   return client.product.fetchMultiple(ids)
 }
 
 function fetchProductsByQuery(params, client) {
-   return client.product.fetchQuery(params);
+   return client.product.fetchQuery(params)
 }
 
 function findVariantFromSelectedOptions(product, selectedOptions) {
-   return buildClient().product.helpers.variantForOptions(product, selectedOptions);
+   return buildClient().product.helpers.variantForOptions(product, selectedOptions)
 }
 
 function fetchAllProducts(client) {
-   return client.product.fetchAll();
+   return client.product.fetchAll()
 }
 
 function fetchNextPage(items, client) {
-   return client.fetchNextPage(items);
+   return client.fetchNextPage(items)
 }
-
 
 /*
 
@@ -38,37 +35,27 @@ Convience wrappers
 
 */
 function getProduct(id) {
-   return fetchProductByID(id, buildClient());
+   return fetchProductByID(id, buildClient())
 }
 
 async function getProducts(ids = []) {
-
-   return fetchProductsByIDs(ids, buildClient());
-
+   return fetchProductsByIDs(ids, buildClient())
 }
 
 function getAllProducts() {
-   return fetchAllProducts(buildClient());
+   return fetchAllProducts(buildClient())
 }
 
 function queryProducts(params) {
-   return fetchProductsByQuery(params, buildClient());
+   return fetchProductsByQuery(params, buildClient())
 }
 
 function getNextPage(items) {
-   return fetchNextPage(items, buildClient());
+   return fetchNextPage(items, buildClient())
 }
 
 function getAllTags() {
-   return fetchAllTags();
+   return fetchAllTags()
 }
 
-export {
-   getProduct,
-   getProducts,
-   getAllProducts,
-   queryProducts,
-   getNextPage,
-   getAllTags,
-   findVariantFromSelectedOptions
-}
+export { getProduct, getProducts, getAllProducts, queryProducts, getNextPage, getAllTags, findVariantFromSelectedOptions }
