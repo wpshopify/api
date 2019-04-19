3f1cdbfac65b67f0394cf01adf767fa589e54a4b
Andrew Robbins <arobbins@simpleblend.net>
2019-04-18 21:47:40 -0500

    Updated import

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ export { buildCheckout, buildInstances, getCheckoutID } from './src/checkout'
 
 export { addLineItems } from './src/checkout'
 
-export { getProduct, getProductsFromIds, queryProducts, getAllProducts, getNextPage, findVariantFromSelectedOptions } from './src/products'
+export { getProduct, getProductsFromIds, queryProducts, getAllProducts, getNextPage, findVariantFromSelectedOptions, fetchByCollectionTitle } from './src/products'
 
 export { getShopInfo } from './src/shop'
 export { fetchNextPage } from './src/pagination'
 export { fetchByTitleParams, buildFetchQueryParams, fetchByQueryParams } from './src/api'
 
-export { getAllTags, getFilterData, getProductsFromQuery, fetchByCollectionTitle } from './src/internal/products'
+export { getAllTags, getFilterData, getProductsFromQuery } from './src/internal/products'
 export { getComponentOptions } from './src/internal/components'
 
 export { setCache, getCache, deleteCache } from './src/cache'
