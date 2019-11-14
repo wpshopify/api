c8c9d74c84530418609a7d0fa7d4539c08b89119
Andrew Robbins <arobbins@simpleblend.net>
2019-11-14 17:35:22 -0600

    Updated builder tool

--- src/products/index.js
+++ src/products/index.js
@@ -1,24 +1,27 @@
-import { buildClient } from '../client'
-import { maybeAlterErrorMessage } from '../errors'
-import has from 'lodash/has'
-import isString from 'lodash/isString'
-import to from 'await-to-js'
-import { isArray } from 'util'
+import { buildClient } from "../client"
+import { maybeAlterErrorMessage } from "../errors"
+import has from "lodash/has"
+import isString from "lodash/isString"
+import to from "await-to-js"
+import { isArray } from "util"
 
 function fetchProductByID(id, client) {
-   return client.product.fetch(id)
+  return client.product.fetch(id)
 }
 
 function fetchProductsByIDs(ids, client) {
-   return client.product.fetchMultiple(ids)
+  return client.product.fetchMultiple(ids)
 }
 
 function fetchProductsByQuery(params, client) {
-   return client.product.fetchQuery(params)
+  return client.product.fetchQuery(params)
 }
 
 function findVariantFromSelectedOptions(product, selectedOptions) {
-   return buildClient().product.helpers.variantForOptions(product, selectedOptions)
+  return buildClient().product.helpers.variantForOptions(
+    product,
+    selectedOptions
+  )
 }
 
 /*
@@ -27,84 +30,84 @@ Add product fields to the GQL query
 
 */
 function addProductFields(product) {
-   product.add('id')
-   product.add('title')
-   product.add('availableForSale')
-   product.add('createdAt')
-   product.add('description')
-   product.add('descriptionHtml')
-   product.add('handle')
-   product.add('onlineStoreUrl')
-   product.add('productType')
-   product.add('publishedAt')
-   product.add('updatedAt')
-   product.add('vendor')
+  product.add("id")
+  product.add("title")
+  product.add("availableForSale")
+  product.add("createdAt")
+  product.add("description")
+  product.add("descriptionHtml")
+  product.add("handle")
+  product.add("onlineStoreUrl")
+  product.add("productType")
+  product.add("publishedAt")
+  product.add("updatedAt")
+  product.add("vendor")
 
-   product.addConnection('images', { args: { first: 250 } }, image => {
-      image.add('altText')
-      image.add('src')
-   })
+  product.addConnection("images", { args: { first: 250 } }, image => {
+    image.add("altText")
+    image.add("src")
+  })
 
-   product.add('options', option => {
-      option.add('name')
-      option.add('values')
-   })
+  product.add("options", option => {
+    option.add("name")
+    option.add("values")
+  })
 
-   //
-   // Only works if they are whitelisted first
-   //
-   // product.addConnection('metafields', { args: { first: 1 } }, metafield => {
-   //    metafield.add('namespace')
-   //    metafield.add('key')
-   //    metafield.add('value')
-   // })
+  //
+  // Only works if they are whitelisted first
+  //
+  // product.addConnection('metafields', { args: { first: 1 } }, metafield => {
+  //    metafield.add('namespace')
+  //    metafield.add('key')
+  //    metafield.add('value')
+  // })
 
-   product.addConnection('variants', { args: { first: 250 } }, variants => {
-      variants.add('id')
+  product.addConnection("variants", { args: { first: 250 } }, variants => {
+    variants.add("id")
 
-      variants.add('product', options => {
-         options.add('id')
-         options.add('title')
-      })
+    variants.add("product", options => {
+      options.add("id")
+      options.add("title")
+    })
 
-      variants.add('title')
-      variants.add('price')
-      // variants.add('priceV2')
-      variants.add('availableForSale')
-      variants.add('compareAtPrice')
-      // variants.add('compareAtPriceV2')
-      variants.add('sku')
-      variants.add('weight')
+    variants.add("title")
+    variants.add("price")
+    // variants.add('priceV2')
+    variants.add("availableForSale")
+    variants.add("compareAtPrice")
+    // variants.add('compareAtPriceV2')
+    variants.add("sku")
+    variants.add("weight")
 
-      variants.add('selectedOptions', options => {
-         options.add('name')
-         options.add('value')
-      })
+    variants.add("selectedOptions", options => {
+      options.add("name")
+      options.add("value")
+    })
 
-      variants.add('image', image => {
-         image.add('src')
-         image.add('id')
-         image.add('altText')
-      })
-   })
+    variants.add("image", image => {
+      image.add("src")
+      image.add("id")
+      image.add("altText")
+    })
+  })
 }
 
 function addCollectionFields(collection, connectionParams) {
-   collection.add('id')
-   collection.add('title')
-   collection.add('description')
-   collection.add('descriptionHtml')
-   collection.add('handle')
-   collection.add('updatedAt')
+  collection.add("id")
+  collection.add("title")
+  collection.add("description")
+  collection.add("descriptionHtml")
+  collection.add("handle")
+  collection.add("updatedAt")
 
-   collection.add('image', image => {
-      image.add('id')
-      image.add('altText')
-      image.add('src')
-      image.add('originalSrc')
-   })
+  collection.add("image", image => {
+    image.add("id")
+    image.add("altText")
+    image.add("src")
+    image.add("originalSrc")
+  })
 
-   /*
+  /*
    
    sortKey values:
 
@@ -121,191 +124,211 @@ function addCollectionFields(collection, connectionParams) {
 
    */
 
-   if (connectionParams) {
-      var productsArgs = {
-         first: connectionParams.first,
-         sortKey: connectionParams.sortKey,
-         reverse: connectionParams.reverse
-      }
+  if (connectionParams) {
+    var productsArgs = {
+      first: connectionParams.first,
+      sortKey: connectionParams.sortKey,
+      reverse: connectionParams.reverse
+    }
 
-      collection.addConnection('products', { args: productsArgs }, product => {
-         addProductFields(product)
-      })
-   }
+    collection.addConnection("products", { args: productsArgs }, product => {
+      addProductFields(product)
+    })
+  }
 }
 
 function enumValue(client, params) {
-   return client.graphQLClient.enum(params.sortKey)
+  return client.graphQLClient.enum(params.sortKey)
 }
 
 function maybeUppercaseSortKey(sortKey) {
-   if (isString(sortKey)) {
-      return sortKey.toUpperCase()
-   }
+  if (isString(sortKey)) {
+    return sortKey.toUpperCase()
+  }
 
-   return sortKey
+  return sortKey
 }
 
 function refetchQuery(node) {
-   const client = buildClient()
+  const client = buildClient()
 
-   return client.graphQLClient.refetch(node)
+  return client.graphQLClient.refetch(node)
 }
 
 function hasValidCredentials(client) {
-   if (has(client, 'type') || has(client, 'message')) {
-      return false
-   }
+  if (has(client, "type") || has(client, "message")) {
+    return false
+  }
 
-   if (client.config.domain && client.config.storefrontAccessToken) {
-      return true
-   }
+  if (client.config.domain && client.config.storefrontAccessToken) {
+    return true
+  }
 
-   return false
+  return false
 }
 
 function graphQuery(type, queryParams, connectionParams = false) {
-
-   return new Promise(async (resolve, reject) => {
-      
-      if (!queryParams) {
-         return reject(maybeAlterErrorMessage('Uh oh, it looks your query params are invalid. Please clear your browser cache and reload the page.'))
-      }
-
-      const client = buildClient()
-
-
-      if (!hasValidCredentials(client)) {
-         return reject(maybeAlterErrorMessage('You still need to connect your Shopify store or the credentials are missing. Double check the "connect" tab within the plugin settings.'))
-      }
-
-      if (has(queryParams, 'sortKey')) {
-         queryParams.sortKey = maybeUppercaseSortKey(queryParams.sortKey)
-         queryParams.sortKey = enumValue(client, queryParams)
-      }
-
-      if (has(connectionParams, 'sortKey')) {
-         connectionParams.sortKey = maybeUppercaseSortKey(connectionParams.sortKey)
-         connectionParams.sortKey = enumValue(client, connectionParams)
-      }
-
-      // Defaults to 10
-      if (!has(queryParams, 'first') && !has(queryParams, 'last')) {
-         queryParams.first = 10
-      }
-
-      const [requestError, response] = await to(
-         client.graphQLClient.send(
-            client.graphQLClient.query(root => {
-               resourceQuery(root, type, queryParams, connectionParams)
-            })
-         )
+  return new Promise(async (resolve, reject) => {
+    if (!queryParams) {
+      return reject(
+        maybeAlterErrorMessage(
+          "Uh oh, it looks your query params are invalid. Please clear your browser cache and reload the page."
+        )
       )
+    }
 
-      if (requestError) {
-         return reject(maybeAlterErrorMessage(requestError))
-      }
+    const client = buildClient()
 
-      if (has(response, 'errors')) {
-         return reject(maybeAlterErrorMessage(response.errors))
-      }
+    if (!hasValidCredentials(client)) {
+      return reject(
+        maybeAlterErrorMessage(
+          'You still need to connect your Shopify store or the credentials are missing. Double check the "connect" tab within the plugin settings.'
+        )
+      )
+    }
 
-      return resolve(response)
-   })
+    console.log("..... queryParams", queryParams)
+
+    if (has(queryParams, "sortKey")) {
+      queryParams.sortKey = maybeUppercaseSortKey(queryParams.sortKey)
+      queryParams.sortKey = enumValue(client, queryParams)
+    }
+
+    if (has(connectionParams, "sortKey")) {
+      connectionParams.sortKey = maybeUppercaseSortKey(connectionParams.sortKey)
+      connectionParams.sortKey = enumValue(client, connectionParams)
+    }
+
+    // Defaults to 10
+    if (!has(queryParams, "first") && !has(queryParams, "last")) {
+      queryParams.first = 10
+    }
+
+    if (queryParams.query === undefined) {
+      queryParams.query = "*"
+    }
+
+    const [requestError, response] = await to(
+      client.graphQLClient.send(
+        client.graphQLClient.query(root => {
+          resourceQuery(root, type, queryParams, connectionParams)
+        })
+      )
+    )
+
+    if (requestError) {
+      return reject(maybeAlterErrorMessage(requestError))
+    }
+
+    if (has(response, "errors")) {
+      return reject(maybeAlterErrorMessage(response.errors))
+    }
+
+    return resolve(response)
+  })
 }
 
 function resourceQuery(root, type, queryParams, connectionParams = false) {
-   switch (type) {
-      case 'products':
-         productsQuery(root, queryParams)
-         break
+  switch (type) {
+    case "products":
+      productsQuery(root, queryParams)
+      break
 
-      case 'collections':
-         collectionsQuery(root, queryParams, connectionParams)
-         break
+    case "collections":
+      collectionsQuery(root, queryParams, connectionParams)
+      break
 
-      default:
-         break
-   }
+    default:
+      break
+  }
 }
 
 function productsQuery(root, queryParams) {
-   root.addConnection('products', { args: queryParams }, product => {
-      addProductFields(product)
-   })
+  root.addConnection("products", { args: queryParams }, product => {
+    addProductFields(product)
+  })
 }
 
 function collectionsQuery(root, queryParams, connectionParams = false) {
-   root.addConnection('collections', { args: queryParams }, collection => {
-      addCollectionFields(collection, connectionParams)
-   })
+  root.addConnection("collections", { args: queryParams }, collection => {
+    addCollectionFields(collection, connectionParams)
+  })
 }
 
 function refetchLineItems(ids, client) {
-   return new Promise(async (resolve, reject) => {
-      if (!ids || !isArray(ids)) {
-         return reject('No product ids found')
+  return new Promise(async (resolve, reject) => {
+    if (!ids || !isArray(ids)) {
+      return reject("No product ids found")
+    }
+
+    var allPromises = ids.map(async id => {
+      const [err, succ] = await to(fetchProductByID(id, client))
+
+      if (!err) {
+        return succ
       }
+      return err
+    })
 
-      var allPromises = ids.map(async id => {
-         const [err, succ] = await to(fetchProductByID(id, client))
+    var [allPromiseError, allPromiseResponse] = await to(
+      Promise.all(allPromises)
+    )
 
-         if (!err) {
-            return succ
-         }
-         return err
-      })
+    if (allPromiseError) {
+      reject(allPromiseError)
+    }
 
-      var [allPromiseError, allPromiseResponse] = await to(Promise.all(allPromises))
-
-      if (allPromiseError) {
-         reject(allPromiseError)
-      }
-
-      resolve(allPromiseResponse)
-   })
+    resolve(allPromiseResponse)
+  })
 }
 
 function isTypeError(data) {
-   return data instanceof Error
+  return data instanceof Error
 }
 
 function withoutTypeErrors(results) {
-   return results.filter(result => !isTypeError(result))
+  return results.filter(result => !isTypeError(result))
 }
 
 function getProductsFromIds(ids = []) {
-   return new Promise(async (resolve, reject) => {
-      const client = buildClient()
+  return new Promise(async (resolve, reject) => {
+    const client = buildClient()
 
-      const [productsError, products] = await to(fetchProductsByIDs(ids, client))
+    const [productsError, products] = await to(fetchProductsByIDs(ids, client))
 
-      /*
+    /*
       
       If productsError, most likely the product was hidden from the sales channel
 
       */
-      if (productsError) {
-         const [refetchError, results] = await to(refetchLineItems(ids, client))
+    if (productsError) {
+      const [refetchError, results] = await to(refetchLineItems(ids, client))
 
-         if (refetchError) {
-            console.error('wpshopify error 2 💩 ', refetchError)
-            return reject(maybeAlterErrorMessage(refetchError))
-         }
-
-         return resolve(withoutTypeErrors(results))
+      if (refetchError) {
+        console.error("wpshopify error 2 💩 ", refetchError)
+        return reject(maybeAlterErrorMessage(refetchError))
       }
 
-      return resolve(products)
-   })
+      return resolve(withoutTypeErrors(results))
+    }
+
+    return resolve(products)
+  })
 }
 
 function queryProducts(params) {
-   return fetchProductsByQuery(params, buildClient())
+  return fetchProductsByQuery(params, buildClient())
 }
 
 function getAllTags() {
-   return fetchAllTags()
+  return fetchAllTags()
 }
 
-export { getProductsFromIds, queryProducts, getAllTags, findVariantFromSelectedOptions, graphQuery, refetchQuery }
+export {
+  getProductsFromIds,
+  queryProducts,
+  getAllTags,
+  findVariantFromSelectedOptions,
+  graphQuery,
+  refetchQuery
+}
