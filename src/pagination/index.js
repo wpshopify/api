bf1712413ac93d4e788c8d179dbb7e088f4203f1
Andrew Robbins <arobbins@simpleblend.net>
2020-02-18 16:47:10 -0600

    Refactored update_payload reducer

--- src/pagination/index.js
+++ src/pagination/index.js
@@ -6,7 +6,9 @@ Direct API functions
 
 */
 function fetchNextPage(payload) {
-   return buildClient().fetchNextPage(payload)
+  console.log('payloadpayloadpayload for next', payload)
+
+  return buildClient().fetchNextPage(payload)
 }
 
 export { fetchNextPage }
