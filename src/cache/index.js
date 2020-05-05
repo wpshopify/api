873ea9bb53da456aa09fb671ebc214dfad76c1b1
Andrew Robbins <arobbins@simpleblend.net>
2020-05-05 16:34:08 -0500

    3.0 Updates

--- src/cache/index.js
+++ src/cache/index.js
@@ -2,27 +2,27 @@ import store from 'store'
 import startsWith from 'lodash/startsWith'
 
 function setCache(name, value) {
-   return store.set(name, value)
+  return store.set(name, value)
 }
 
 function getCache(name) {
-   return store.get(name)
+  return store.get(name)
 }
 
 function deleteCache(name = false) {
-   if (!name) {
-      return store.clearAll()
-   }
+  if (!name) {
+    return store.clearAll()
+  }
 
-   return store.remove(name)
+  return store.remove(name)
 }
 
 function deleteCacheContains(contains) {
-   return store.each(function(value, key) {
-      if (startsWith(key, contains)) {
-         store.remove(key)
-      }
-   })
+  return store.each(function (value, key) {
+    if (startsWith(key, contains)) {
+      store.remove(key)
+    }
+  })
 }
 
 export { setCache, getCache, deleteCache, deleteCacheContains }
